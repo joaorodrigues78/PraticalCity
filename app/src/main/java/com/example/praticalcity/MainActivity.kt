464f6a2507462b9834bd222b67be0a23903868af
João Rodrigues <jcarlosrodrigues@ipvc.pt>
2021-03-13 17:15:49 +0000

    Notas - Primeiro commit, botão da página inicial muda para atividade das notas

--- app/src/main/java/com/example/praticalcity/MainActivity.kt
+++ app/src/main/java/com/example/praticalcity/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.praticalcity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_notas = findViewById<Button>(R.id.button_notas)
+
+        //botão do login redireciona para a atividade das notas
+        button_notas.setOnClickListener{
+            val intent = Intent(this, notas::class.java)
+            startActivity(intent)
+        }
     }
-
-
-
 }
