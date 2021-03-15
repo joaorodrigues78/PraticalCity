ccd365c1d7c5a3e0711b0631068174e6b69dc70d
João Rodrigues <jcarlosrodrigues@ipvc.pt>
2021-03-15 20:53:51 +0000

    Notas - Segundo commit: Desenvolvido as entidades, foi criado o DAO, adicionada room database, criado o repositório e ViewModel. Foi também adicionada a recyclerview. O código apresenta 1 erro com a seguinte descrição: "Execution failed for task ':app:kaptDebugKotlin'. > A failure occurred while executing org.jetbrains.kotlin.gradle.internal.KaptExecution  > java.lang.reflect.InvocationTargetException (no error message)"

--- app/src/main/java/com/example/praticalcity/notas.kt
+++ app/src/main/java/com/example/praticalcity/notas.kt
@@ -1,11 +1,43 @@
 package com.example.praticalcity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.praticalcity.adapters.NotasAdapter
+import com.example.praticalcity.viewModel.NotasViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class notas : AppCompatActivity() {
+
+    private lateinit var notasViewModel: NotasViewModel
+    private val newNotaActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notas)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = NotasAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //ViewModel
+        notasViewModel = ViewModelProvider(this).get(NotasViewModel::class.java)
+        notasViewModel.allNotas.observe(this, {
+                notas -> notas?.let{adapter.setNotas(it)}
+        })
+
+        //Fab
+        /*
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener(
+            val intent = Intent(this@notas, AddNota::class.java)
+            startActivityForResult(intent, newNotaActivityRequestCode)
+        )
+        */
+
     }
 }
