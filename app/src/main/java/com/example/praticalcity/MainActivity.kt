95760a87bbc36388a393040d7b935185c54a2766
João Rodrigues <jcarlosrodrigues@ipvc.pt>
2021-03-31 16:10:17 +0100

    Master - Foi feito o Merde da Notas com o Master

--- app/src/main/java/com/example/praticalcity/MainActivity.kt
+++ app/src/main/java/com/example/praticalcity/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.praticalcity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+        val button_notas = findViewById<Button>(R.id.button_notas)
+
+        //botão do login redireciona para a atividade das notas
+        button_notas.setOnClickListener{
+            val intent = Intent(this, Notas::class.java)
+            startActivity(intent)
+        }
+
+    }
 
 
 }
