135722dfb48f4af5118c16ada47d763790f9d7aa
João Rodrigues <jcarlosrodrigues@ipvc.pt>
2021-05-03 18:24:05 +0100

    Adicionado mapa e criado menu para utilizador após o login

--- app/src/main/java/com/example/praticalcity/MapsActivity.kt
+++ app/src/main/java/com/example/praticalcity/MapsActivity.kt
@@ -1,26 +1,72 @@
 package com.example.praticalcity
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.praticalcity.api.EndPoints
+import com.example.praticalcity.api.ServiceBuilder
+import com.example.praticalcity.api.Situacao
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.example.praticalcity.api.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var  situacoes: List<Situacao>
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val request = ServiceBuilder.buildService(EndPoints::class.java)
+        val call = request.getSituacoes()
+        var position: LatLng
+
+        call.enqueue(object : Callback<List<Situacao>> {
+            override fun onResponse(call: Call<List<Situacao>>, response: Response<List<Situacao>>){
+                if (response.isSuccessful){
+                    situacoes = response.body()!!
+                    for (situacao in situacoes){
+                        position = LatLng(situacao.latitude.toDouble(), situacao.longitude.toDouble())
+                        mMap.addMarker(MarkerOptions().position(position).title(situacao.titulo + "-" + situacao.descr))
+
+                    }
+                }
+            }
+            override fun onFailure(call: Call<List<Situacao>>, t: Throwable){
+                Toast.makeText(this@MapsActivity,"${t.message}", Toast.LENGTH_LONG).show()
+            }
+        })
+
     }
 
     /**
@@ -36,8 +82,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //val sydney = LatLng(-34.0, 151.0)
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        //mMap.setOnInfoWindowClickListener(this)
+        setUpMap()
     }
+
+    private fun setUpMap() {
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+
+            return
+        } else{
+            //1
+            mMap.isMyLocationEnabled = true
+
+            //2
+            fusedLocationClient.lastLocation.addOnSuccessListener(this) {location ->
+                //3
+                if(location != null){
+                    lastLocation = location
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                }
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/praticalcity/api/Situacao.kt
+++ app/src/main/java/com/example/praticalcity/api/Situacao.kt
@@ -2,8 +2,8 @@ package com.example.praticalcity.api
 
 data class Situacao(
     val id: Int,
-    val latitude: Double,
-    val longitude: Double,
+    val latitude: Float,
+    val longitude: Float,
     val titulo: String,
     val descr: String,
     val foto: String
