bfe078a20494bcb5a2a11fcf196a34ed6131d2f9
João Rodrigues <jcarlosrodrigues@ipvc.pt>
2021-05-03 11:51:51 +0100

    Retrofit implementado com login funcional mas a não listar problemas

--- app/src/main/java/com/example/praticalcity/adapters/SituacoesAdapter.kt
+++ app/src/main/java/com/example/praticalcity/adapters/SituacoesAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.praticalcity.adapters
 
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -9,18 +8,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.praticalcity.R
 import com.example.praticalcity.api.Situacao
 
-class SituacoesAdapter (val situacoes: List<Situacao>) : RecyclerView.Adapter<SituacaoViewHolder>(){
+class SituacoesAdapter (val situacao: List<Situacao>) : RecyclerView.Adapter<SituacaoViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SituacaoViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_itemsituacoes,parent, false)
         return SituacaoViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: SituacaoViewHolder, position: Int) {
-        return holder.bind(situacoes[position])
+        return holder.bind(situacao[position])
     }
 
     override fun getItemCount(): Int {
-        return situacoes.size
+        return situacao.size
     }
 
 }
@@ -30,14 +29,12 @@ class SituacaoViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
     val longitude: TextView = itemView.findViewById(R.id.longitude)
     val titulo: TextView = itemView.findViewById(R.id.titulo)
     val descr: TextView = itemView.findViewById(R.id.descr)
-    val foto: TextView = itemView.findViewById(R.id.foto)
 
     fun bind(situacao: Situacao){
         latitude.text = situacao.latitude.toString()
         longitude.text = situacao.longitude.toString()
         titulo.text = situacao.titulo
         descr.text = situacao.descr
-        foto.text = situacao.foto
     }
 
 }
--- app/src/main/java/com/example/praticalcity/situacoes.kt
+++ app/src/main/java/com/example/praticalcity/situacoes.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telecom.Call
 import android.view.View
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,7 +14,8 @@ import com.example.praticalcity.api.EndPoints
 import com.example.praticalcity.api.ServiceBuilder
 import com.example.praticalcity.api.Situacao
 import retrofit2.Response
-import javax.security.auth.callback.Callback
+import retrofit2.Call
+import retrofit2.Callback
 
 class situacoes : AppCompatActivity() {
 
@@ -25,19 +24,14 @@ class situacoes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_situacoes)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         shared_preferences = getSharedPreferences("shared_preferences", Context.MODE_PRIVATE)
 
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getSituacoes()
         var recyclerView = findViewById<RecyclerView>(R.id.recyclerview_problema)
 
-        call.enqueue(object : retrofit2.Callback<List<Situacao>> {
-            override fun onResponse(call: retrofit2.Call<List<Situacao>>, response: Response<List<Situacao>>){
+        call.enqueue(object : Callback<List<Situacao>> {
+            override fun onResponse(call: Call<List<Situacao>>, response: Response<List<Situacao>>){
                 if (response.isSuccessful){
                     recyclerView.apply {
                         setHasFixedSize(true)
@@ -46,26 +40,24 @@ class situacoes : AppCompatActivity() {
                     }
                 }
             }
-            override fun onFailure(call: retrofit2.Call<List<Situacao>>, t: Throwable){
+            override fun onFailure(call: Call<List<Situacao>>, t: Throwable){
                 Toast.makeText(this@situacoes,"${t.message}", Toast.LENGTH_LONG).show()
             }
         })
-
-
     }
 
     fun getSingle(view: View){
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getSituacaoById(1)
 
-        call.enqueue(object : retrofit2.Callback<Situacao>{
-            override fun onResponse(call: retrofit2.Call<Situacao>, response: Response<Situacao>){
+        call.enqueue(object : Callback<Situacao>{
+            override fun onResponse(call: Call<Situacao>, response: Response<Situacao>){
                 if (response.isSuccessful){
                     val c: Situacao = response.body()!!
                     Toast.makeText(this@situacoes,"Situacao obtida", Toast.LENGTH_SHORT).show()
                 }
             }
-            override fun onFailure(call: retrofit2.Call<Situacao>, t: Throwable){
+            override fun onFailure(call: Call<Situacao>, t: Throwable){
                 Toast.makeText(this@situacoes,"${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
